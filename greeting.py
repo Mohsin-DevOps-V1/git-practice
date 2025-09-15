82fa37cdc4dd9bc57600ebca9627ee90ece47e7b
Abdulmohsin <Abdulmohsinpatel@outlook.com>
2025-09-15 14:19:12 +0300

    Local: Add greeting msg

---
+++ greeting.py
@@ -0,0 +1,8 @@
+def say_hello(name):
+    """Greets a person using their name."""
+    messsage = f"Hello, {name}!"
+    print(messsage)
+
+if __name__ == "__main__":
+    user_name = "Alice"
+    say_hello(user_name)
