812e0d07ca37a05dfbfac7da8c04d0f7924818f7
Abdulmohsin <Abdulmohsinpatel@outlook.com>
2025-09-15 15:37:17 +0300

    Local: Add voting script - voting.py

---
+++ voting.py
@@ -0,0 +1,9 @@
+def can_vote(age):
+    """Checks if someone is old enough to vote (18+)."""
+    if age > 18:  
+        print("You are eligible to vote.")
+    else:
+        print("You are not eligible to vote yet.")
+
+if __name__ == '__main__':
+    can_vote(18)  
