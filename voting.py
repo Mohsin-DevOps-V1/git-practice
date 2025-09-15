b46508342b3abde2e226428aa51bb0ab93cd225e
Abdulmohsin <Abdulmohsinpatel@outlook.com>
2025-09-15 15:46:44 +0300

    Local: fix the bug on line 3 - voting.py

--- voting.py
+++ voting.py
@@ -1,6 +1,6 @@
 def can_vote(age):
     """Checks if someone is old enough to vote (18+)."""
-    if age > 18:  
+    if age >= 18:  
         print("You are eligible to vote.")
     else:
         print("You are not eligible to vote yet.")
